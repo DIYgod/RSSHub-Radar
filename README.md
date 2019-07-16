16dda13ede9c62e3ba97b5d7f8a0dc6778314fef
DIYgod <diy.d.god@gmail.com>
2019-07-17 01:01:03 +0800

    chore: init workflow

---
+++ README.md
@@ -0,0 +1,20 @@
+# RSSHub-radar
+
+> Browser extension that detects available RSS and [RSSHub](https://github.com/DIYgod/RSSHub) in current page
+
+## 介绍
+
+探测当前页面可用的 RSS 和 [RSSHub](https://github.com/DIYgod/RSSHub) 的浏览器扩展
+
+[Telegram 群](https://t.me/rsshub)
+
+## 相关项目
+
+-   [RSSHub](https://github.com/DIYgod/RSSHub)
+
+## Author
+
+**RSSHub-radar** © [DIYgod](https://github.com/DIYgod), Released under the [MIT](./LICENSE) License.<br>
+Authored and maintained by DIYgod with help from contributors ([list](https://github.com/DIYgod/RSSHub-radar/contributors)).
+
+> Blog [@DIYgod](https://diygod.me) · GitHub [@DIYgod](https://github.com/DIYgod) · Twitter [@DIYgod](https://twitter.com/DIYgod) · Telegram Channel [@awesomeDIYgod](https://t.me/awesomeDIYgod)
---
+++ webpack.config.js
@@ -0,0 +1,106 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+
+    mode: 'production',
+
+    bail: true,
+
+    devtool: 'source-map',
+
+    entry: {
+        'rsshub-radar': './src/js/index.js',
+    },
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+        publicPath: '/',
+    },
+
+    resolve: {
+        modules: ['node_modules'],
+        extensions: ['.js', '.less'],
+    },
+
+    module: {
+        strictExportPresence: true,
+        rules: [
+            {
+                test: /\.js$/,
+                enforce: 'pre',
+                loader: 'eslint-loader',
+                include: path.resolve(__dirname, '../src/js'),
+            },
+            {
+                test: /\.js$/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            cacheDirectory: true,
+                            presets: ['@babel/preset-env']
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 2,
+                        },
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            config: {
+                                path: path.join(__dirname, 'postcss.config.js')
+                            }
+                        }
+                    },
+                    {
+                        loader: 'less-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.(png|jpg)$/,
+                loader: 'url-loader',
+                options: {
+                    'limit': 40000
+                }
+            },
+            {
+                test: /\.svg$/,
+                loader: 'svg-inline-loader'
+            },
+        ]
+    },
+
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
+        new CopyPlugin([
+            {
+                from: 'src/assets',
+                to: '',
+                copyUnmodified: true,
+            },
+        ]),
+    ],
+
+    node: {
+        dgram: 'empty',
+        fs: 'empty',
+        net: 'empty',
+        tls: 'empty',
+    },
+
+};
