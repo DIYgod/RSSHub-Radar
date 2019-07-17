e347a20ff6c82716f6b8bf74def0583a45f41868
DIYgod <diy.d.god@gmail.com>
2019-07-18 01:02:51 +0800

    fix: only handle in active tab

--- src/js/background.js
+++ src/js/background.js
@@ -10,6 +10,10 @@ function handlePageRSS (feeds) {
     }
 }
 
+chrome.browserAction.setBadgeBackgroundColor({
+    color: '#FF2800',
+});
+
 chrome.tabs.onActivated.addListener(function (tab) {
     chrome.tabs.sendMessage(tab.tabId, {
         text: 'getPageRSS',
@@ -17,7 +21,7 @@ chrome.tabs.onActivated.addListener(function (tab) {
 });
 
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-    if (msg.text === 'setPageRSS') {
+    if (msg.text === 'setPageRSS' && sender.tab.active) {
         handlePageRSS(msg.feeds);
     }
 });
