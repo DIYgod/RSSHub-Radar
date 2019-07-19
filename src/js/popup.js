ad3717ace415a981b0bb660cef888ea701bceac2
DIYgod <diy.d.god@gmail.com>
2019-07-19 17:33:12 +0800

    feat: popup style and button

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,16 +1,19 @@
 import '../css/popup.less';
+import ClipboardJS from 'clipboard';
 
-const background = chrome.extension.getBackgroundPage();
-
-function generateList (list) {
+function generateList (type, list) {
     let result = '';
     if (list && list.length) {
+        document.querySelector(`.${type}`).style.display = 'block';
         list.forEach((item) => {
             result += `
-            <li>
-                <img src="${item.image}">
-                <div class="title">${item.title}</div>
-                <a class="url" href="${item.url}">${item.url}</a>
+            <li class="rss-item">
+                <img class="rss-image" src="${item.image}">
+                <div class="rss-info">
+                    <div class="rss-title">${item.title}</div>
+                    <div class="rss-url">${item.url}</div>
+                </div>
+                <div class="rss-action rss-copy" data-clipboard-text="${item.url}">复制</div>
             </li>
             `
         });
@@ -18,4 +21,21 @@ function generateList (list) {
     return result;
 }
 
-document.querySelector('.page-rss ul').innerHTML = generateList(background.pageRSS);
+const background = chrome.extension.getBackgroundPage();
+document.querySelector('.page-rss ul').innerHTML = generateList('page-rss', background.pageRSS);
+
+const clipboard = new ClipboardJS('.rss-copy');
+clipboard.on('success', function(e) {
+    e.trigger.innerHTML = '已复制';
+    setTimeout(() => {
+        e.trigger.innerHTML = '复制';
+    }, 1000);
+});
+
+document.querySelectorAll('.rss-info').forEach((ele) => {
+    ele.addEventListener('click', () => {
+        chrome.tabs.create({
+            url: ele.querySelector('.rss-url').innerHTML,
+        });
+    });
+});
