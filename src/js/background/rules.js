f326328da50a4e987743bf54020895fc4154f314
DIYgod <diy.d.god@gmail.com>
2019-07-21 18:54:09 +0800

    feat: github rules

--- src/js/background/rules.js
+++ src/js/background/rules.js
@@ -103,4 +103,47 @@ module.exports = {
             source: '/playlist',
         }],
     },
+    'github.com': {
+        '.': [{
+            title: 'GitHub 用户仓库',
+            description: 'https://docs.rsshub.app/programming.html#github',
+            source: '/:user',
+            target: '/github/repos/:user',
+        }, {
+            title: 'GitHub Trending',
+            description: 'https://docs.rsshub.app/programming.html#github',
+            source: '/trending',
+            target: '/github/trending/:since',
+        }, {
+            title: 'GitHub 仓库 Issue',
+            description: 'https://docs.rsshub.app/programming.html#github',
+            source: '/:user/:repo/issues',
+            target: '/github/issue/:user/:repo',
+        }, {
+            title: 'GitHub 仓库 Pull Requests',
+            description: 'https://docs.rsshub.app/programming.html#github',
+            source: '/:user/:repo/pulls',
+            target: '/github/pull/:user/:repo',
+        }, {
+            title: 'GitHub 用户 Followers',
+            description: 'https://docs.rsshub.app/programming.html#github',
+            source: '/:user',
+            target: '/github/user/followers/:user',
+        }, {
+            title: 'GitHub 仓库 Stars',
+            description: 'https://docs.rsshub.app/programming.html#github',
+            source: '/:user/:repo/stargazers',
+            target: '/github/stars/:user/:repo',
+        }, {
+            title: 'GitHub 仓库 Branches',
+            description: 'https://docs.rsshub.app/programming.html#github',
+            source: '/:user/:repo/branches',
+            target: '/github/branches/:user/:repo',
+        }, {
+            title: 'GitHub 文件 Commits',
+            description: 'https://docs.rsshub.app/programming.html#github',
+            source: '/:user/:repo/:branch/*filepath',
+            target: '/github/file/:user/:repo/:branch/:filepath',
+        }],
+    },
 };
