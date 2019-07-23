37643fa33dcd7ac907f2945601a003277363c46c
DIYgod <diy.d.god@gmail.com>
2019-07-23 11:08:53 +0800

    fix: missing url change in spa

--- src/js/background/index.js
+++ src/js/background/index.js
@@ -8,6 +8,16 @@ chrome.tabs.onActivated.addListener(function (tab) {
     });
 });
 
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (changeInfo.url && tab.active) {
+        chrome.tabs.sendMessage(tab.id, {
+            text: 'getPageRSS',
+        }, (feeds) => {
+            handleRSS(feeds, tab.id);
+        });
+    }
+});
+
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     if (msg.text === 'setPageRSS' && sender.tab.active) {
         handleRSS(msg.feeds, sender.tab.id);
