19c50ad35087a3d31e4c5dce61be27af944b15d1
KTachibanaM <whj19931115@gmail.com>
2020-10-14 19:05:13 -0700

    add rule for eventernote

--- src/js/common/radar-rules.js
+++ src/js/common/radar-rules.js
@@ -2012,4 +2012,15 @@ export default {
             },
         ],
     },
+    'eventernote.com': {
+        _name: 'Eventernote',
+        www: [
+            {
+                title: '声优活动及演唱会',
+                docs: 'https://docs.rsshub.app/anime.html#eventernote',
+                source: '/actors/:name/:id/events',
+                target: '/eventernote/actors/:name/:id',
+            },
+        ],
+    },
 };
