6c035a11f03d67a7842b138881701cc0746d2af0
Ricardo Simões <16106645+Ricardo-Simoes@users.noreply.github.com>
2022-07-17 01:57:03 +0100

    Added Portuguese translation (#704)
    * Created Portuguese Europen translation
    
    * Portuguese European pt_PT translation added
    
    * Added import for translations/pt.json 8
    
    Added import for translations/pt.json (Portuguese European pt-PT)

--- src/js/common/i18n.js
+++ src/js/common/i18n.js
@@ -2,11 +2,13 @@ import { I18n } from 'i18n-js';
 import en from '../../translations/en.json';
 import es from '../../translations/es.json';
 import eu from '../../translations/eu.json';
+import pt from '../../translations/pt.json';
 import zh from '../../translations/zh.json';
 const i18n = new I18n({
     en,
     es,
     eu,
+    pt,
     zh,
 });
 i18n.enableFallback = true;
