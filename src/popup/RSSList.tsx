bd7155163134330bc01337f2f9a538bb2df67248
Stephen Zhou <zhouyl8352@gmail.com>
2024-06-13 23:53:30 +0800

    fix: remove useless scroll bar (#1035)
    * fix: remove useless scroll bar
    
    * update

--- src/popup/RSSList.tsx
+++ src/popup/RSSList.tsx
@@ -9,7 +9,7 @@ function RSSList({ type, list }: { type: string; list: RSSData[] }) {
   return (
     <div className="space-y-4">
       <h2 className="text-base font-bold">{chrome.i18n.getMessage(type)}</h2>
-      <ul className="overflow-x-auto">
+      <ul className="overflow-x-auto w-max min-w-full max-w-[700px]">
         {list.map((item) => (
           <RSSItem key={item.url + item.title} item={item} type={type} />
         ))}
